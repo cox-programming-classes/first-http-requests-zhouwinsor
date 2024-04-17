5c2cc79af0a04b6211f86c9e54b254bdc5fd5dc7
zhouwinsor <56124717+zhouwinsor@users.noreply.github.com>
2024-04-17 12:45:17 -0400

    class 4/17/2024

--- Program.cs
+++ Program.cs
@@ -6,16 +6,26 @@ using System.Text;
 using System.Text.Json;
 using CS_First_HTTP_Client;
 
+ScheduleService scheduleService = new(ApiService.Current);
+
 await ApiService.Current.AuthenticateAsync(new("gracie.zhou@winsor.edu", "&!*428okeKKN"));
-var user = await ApiService.Current.SendAsync<UserInfo>(HttpMethod.Get, "api/users/self");
-var classes= await ApiService.Current.SendAsync<Schedule[]>(HttpMethod.Get, "api/schedule/academics");
+
+var schedule = await scheduleService.GetAcademicSchedule();
+
+foreach (var classes in schedule) {
+Console.WriteLine(classes);
+}
+
+//var user = await ApiService.Current.SendAsync<UserInfo>(HttpMethod.Get, "api/users/self");
+//var classes= await ApiService.Current.SendAsync<Schedule[]>(HttpMethod.Get, "api/schedule/academics");
+
 /*foreach (var a in classes){
     Console.WriteLine(a);
 }
-*/
 var cycleDays = await ApiService.Current.SendAsync<CycleDay[]>(HttpMethod.Get, "api/schedule/cycle-day");
 
 foreach (var day in cycleDays)
 {
     Console.WriteLine($"{day.date:yyyy-M-d dddd} is {day.cycleDay}");
 }
+*/
---
+++ Schedule Service.cs
@@ -0,0 +1,18 @@
+using System.Collections.Immutable;
+
+namespace CS_First_HTTP_Client;
+
+public class ScheduleService
+{
+    private readonly ApiService _api; //dependency injection
+
+    public ScheduleService(ApiService api)
+    {
+        _api = api;
+    }
+
+    public async Task <ImmutableArray<Schedule>> GetAcademicSchedule(bool detailed = false)
+    {
+        return await _api.SendAsync<ImmutableArray<Schedule>>(HttpMethod.Get, $"api/schedule/academics?detailed={detailed}");
+    }
+}
